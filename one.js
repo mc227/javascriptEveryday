b607496a0133fb03486475e7f8cab03cb9420aa9
mc227 <markcostales.mark@gmail.com>
2022-04-13 19:29:19 -0400

    twoNumberSum solution 1 O(n^2)T | O(1)S

--- one.js
+++ one.js
@@ -1,35 +1,31 @@
 
-// algo expert solution 1
-// function twoNumberSum(array, targetSum) {
-    
-//     for(let i = 0; i < array.length; i++) {
-//         let firstNum = array[i];
-//         for(let j = i + 1; j < array.length; j++) {
-//             let secondNum = array[j]
-//             if (firstNum + secondNum === targetSum) {
-//                 return [firstNum, secondNum]
-//             }
-//         }
-//     }
-//     return [];
-// }
+/*
+This is algo expert's first solution to twoNumberSum
+supposedly this is O(n^2)T | O(1)S
+*/
+function twoNumberSum(someArray,someSum) {
 
-// my solution 
-function twoNumberSum(array, targetSum) {
-    let output = [];
-    for(let i = 0; i < array.length; i++) {
-        for(let j = 0; j < i; j++) {
-            if (array[i] + array[j] === targetSum) {
-                output.push(array[i])
-                output.push(array[j])
+    // loop through array first time
+    // loop through array second time
+    // sum item in first loop to second loop to check if equals to the Sum
+    // if equal then return 
+    // if not try a new sum.
+    for(let i = 0; i < someArray.length; i++) {
+        let firstNum = someArray[i];
+        for(let j = i + 1; j < someArray.length; j++) {
+            let secondNum = someArray[j];
+            if (firstNum + secondNum === someSum) {
+                return new Array(firstNum, secondNum);
             }
         }
     }
-    return output
+    return [];
 }
 
 
-let rank = new Array(2, 4, 6, 8);
-let targetSum = 14;
-console.log(twoNumberSum(rank, targetSum))
 
+let rank = new Array(1, 2, 6, 8);
+let targetSum = 14;
+// console.log("rank: ",rank)
+// console.log("targetSum: ",targetSum)
+console.log(twoNumberSum(rank, targetSum))
