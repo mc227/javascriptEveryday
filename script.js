a42d96ae78f97f0a1831fb5d0bd754b90b5cd1df
mc227 <markcostales.mark@gmail.com>
2022-07-12 18:30:19 -0400

    functions

--- script.js
+++ script.js
@@ -1,94 +1,142 @@
-// Why did the programmer quit their job?
-// They didn't get arrays (a raise...lol)
+// function definition
+// function hello() {
+//   // function body
+//   return 'Hello';
+// }
 
-const numbers = [1, 2, 3, 4, 5, 6, 7]; // first element is accessible at index 0
-const x = [1, 'b', true, { a: 'hello' }];
-console.log(numbers[1]);
+// console.log(hello());
 
-// const newNumbers = numbers.slice(2, 5); // slice start index, end index
-// console.log(newNumbers)
-// console.log(numbers)
-// console.log(numbers.splice(0, 3)); // start index, length
-// console.log(numbers);
-// console.log(numbers.pop());
-// console.log(numbers);
-// numbers.push(4444)
-// console.log(numbers);
-// numbers.push(123)
-// console.log(numbers)
-// console.log(newNumbers)
-// console.log(numbers);
-// console.log(numbers[3]);
-// console.log(numbers[1])
+// function ohHiMark() {
+//   return 'Oh Hi, Mark.'
+// }
 
-// console.log(numbers.length)
-// delete numbers[1]
-// console.log(numbers.length)
-// console.log(numbers)
+// console.log(ohHiMark())
+// ES6+
+// // plus "template literal syntax"
+// function hello(who, lang = 'en') {
+//   if (lang === 'en') {
+//     // return 'Hello ' + who + '!';
+//     return `Hello ${who}!`
+//   } else if (lang === 'es') {
+//     return 'Hola ' + who + '!';
+//   } else {
+//     return 'Yo ' + who + '!'
+//   }
+// }
+
+// console.log(hello('Sam'));
+
+// function hello(who, lang = 'en') {
+//   if (lang === 'en') {
+//     // return 'Hello ' + who + '!';
+//     return `Hello ${who}!`
+//   } else if (lang === 'es') {
+//     return 'Hola ' + who + '!';
+//   } else {
+//     return 'Oh, Hi ' + who + '!'
+//   }
+// }
+
+// console.log(hello('Mark', "it"));
+
+// function sumMyNumbers() {
+//   console.log(arguments); // lists the args for the function
+//   return Array.prototype.reduce.call(arguments, function(a, b) {
+//     return a + b
+//   })
+// }
+
+// console.log(
+//   sumMyNumbers(1, 2, 3)
+// );
+
+// Rest (...) Params
+// function sumMyNumbers(...numbers) {
+//   // numbers --> Array
+//   console.log(numbers)
+//   return numbers.reduce((sum, element) => sum + element);
+// }
+// console.log(sumMyNumbers(1, 2, 4, 50, 1, 1, 2, 45, 213, 12));
+
+// Spread / Object Destructuring in functions
+const person = {
+  name: 'John',
+  age: 23
+};
+
+// function greet(obj) {
+//   return `Hi ${obj.name}`
+// }
+
+// console.log(greet(person));
 
 
-// const a = numbers.splice(0, 2)
-// console.log(numbers);
-// console.log(numbers.length)
+function greet({ name, age }) { // we know that the parameter is going to be the person object
+  return `Hi ${name} you are ${age} years old`;
+}
 
-// numbers[0] = 10
-// numbers[18] = 15;
-// console.log(numbers)
+console.log(greet(person));
 
-// numbers.push(15)
-// console.log(numbers);
+// function expression (named)
+// const hello = function() {
+//   return 'Hello'
+// };
+// console.log(hello());
 
-// const a = [1, 2];
-// const b = [3, 4];
-// const x = [5, 6];
+// const numbers = [1, 2];
+// const mapperFunction = function(elements) {
+//   return elements + 5;
+// }
+// const newNumbers = numbers.map(mapperFunction);
+// console.log(newNumbers);
 
-// const c = a.concat(b).concat(x);
-// console.log(c)
-// ES6
-// const c = [...a, ...b, ...x];
-// console.log(c);
+// const a = numbers.forEach(mapperFunction);
+// console.log(a);
 
-// adding a new element to the beginning of an array
-// const num = [1, 2];
-// num.unshift(0);
-// console.log(num);
+// const mapperFunction = function(elements) { 
+//   return elements * 2;
+// };
+// const newNumbers = numbers.map(mapperFunction);
+// console.log(newNumbers);
 
-// console.log(numbers);
-// console.log(numbers.at(0))
-// console.log(numbers.at(-1)) // console.log(numbers[numbers.length - 1])
+// const n = [1, 2];
+// // const abc = function(element) {
+// //   console.log(element);
+// // };
+// // n.forEach(abc); // .forEach() does not return anything
 
-// console.log(a.includes(2))
+// const newArray = n.map(function(element) {
+//   return element + 10;
+// }); // .map() returns a new array
+// console.log(newArray);
 
-// ES6 array destructuring
+// const response = {
+//   count: 2,
+//   data: [{
+//     name: 'Luke Skywalker',
+//     films: ['Empire Strikes Back', 'The Force Awakens']
+//   }, {
+//     name: 'Han Solo',
+//     films: ['Empire Strikes Back', 'The Force Awakens']
+//   }]
+// };
 
-const x = [1, 2];
-// const x1 = x[0]; // 1
-// const x2 = x[1]; // 2
+// const filterFn = function(element) {
+//   return element.name === 'Han Solo'
+// };
 
-// const [x1, x2] = x;
-// console.log(x1, x2);
-
-// const names = ['John', 'Kate'];
-// const [name1, name2] = names;
-// console.log(name2)
-
-// map, filter, reduce
+// const hanssolofilms = response.data.filter(filterFn);
+// console.log(hanssolofilms);
 
 // const numbers = [1, 2, 3, 4];
-// const squared = numbers.map(function(element) {
-//   return element * element; 
-// });
-// console.log(squared);
+// return an array of numbers that can be divided by 2
+// const filterFun = function(el) {
+//   return el % 2 === 0
+// }
+// const divisibleBy2 = numbers.filter(filterFun);
+// console.log(divisibleBy2);
 
-// const filtered = numbers.filter(function(element) {
-//   return element % 2 === 0
+// const sum = numbers.reduce(function(prev, current) {
+//   return prev * current
 // })
-// console.log(filtered);
-
-// reducing the values of the array to a single value
-// const summed = numbers.reduce(function(rollingSum, element) {
-//   console.log('element is', element);
-//   console.log('rollingSum is', rollingSum);
-//   return rollingSum * element;
-// });
-// console.log(summed);
+// console.log(sum);
