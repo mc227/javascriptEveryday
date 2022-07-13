fad5d13d38a2036f3e17adbf50bb2b933a96658e
mc227 <markcostales.mark@gmail.com>
2022-07-13 17:39:00 -0400

    variables (var)

--- script.js
+++ script.js
@@ -1,115 +1,3 @@
-// function definition
-// function hello() {
-//   // function body
-//   return 'Hello';
-// }
-
-// console.log(hello());
-
-// function ohHiMark() {
-//   return 'Oh Hi, Mark.'
-// }
-
-// console.log(ohHiMark())
-// ES6+
-// // plus "template literal syntax"
-// function hello(who, lang = 'en') {
-//   if (lang === 'en') {
-//     // return 'Hello ' + who + '!';
-//     return `Hello ${who}!`
-//   } else if (lang === 'es') {
-//     return 'Hola ' + who + '!';
-//   } else {
-//     return 'Yo ' + who + '!'
-//   }
-// }
-
-// console.log(hello('Sam'));
-
-// function hello(who, lang = 'en') {
-//   if (lang === 'en') {
-//     // return 'Hello ' + who + '!';
-//     return `Hello ${who}!`
-//   } else if (lang === 'es') {
-//     return 'Hola ' + who + '!';
-//   } else {
-//     return 'Oh, Hi ' + who + '!'
-//   }
-// }
-
-// console.log(hello('Mark', "it"));
-
-// function sumMyNumbers() {
-//   console.log(arguments); // lists the args for the function
-//   return Array.prototype.reduce.call(arguments, function(a, b) {
-//     return a + b
-//   })
-// }
-
-// console.log(
-//   sumMyNumbers(1, 2, 3)
-// );
-
-// Rest (...) Params
-// function sumMyNumbers(...numbers) {
-//   // numbers --> Array
-//   console.log(numbers)
-//   return numbers.reduce((sum, element) => sum + element);
-// }
-// console.log(sumMyNumbers(1, 2, 4, 50, 1, 1, 2, 45, 213, 12));
-
-// Spread / Object Destructuring in functions
-const person = {
-  name: 'John',
-  age: 23
-};
-
-// function greet(obj) {
-//   return `Hi ${obj.name}`
-// }
-
-// console.log(greet(person));
-
-
-function greet({ name, age }) { // we know that the parameter is going to be the person object
-  return `Hi ${name} you are ${age} years old`;
-}
-
-console.log(greet(person));
-
-// function expression (named)
-// const hello = function() {
-//   return 'Hello'
-// };
-// console.log(hello());
-
-// const numbers = [1, 2];
-// const mapperFunction = function(elements) {
-//   return elements + 5;
-// }
-// const newNumbers = numbers.map(mapperFunction);
-// console.log(newNumbers);
-
-// const a = numbers.forEach(mapperFunction);
-// console.log(a);
-
-// const mapperFunction = function(elements) { 
-//   return elements * 2;
-// };
-// const newNumbers = numbers.map(mapperFunction);
-// console.log(newNumbers);
-
-// const n = [1, 2];
-// // const abc = function(element) {
-// //   console.log(element);
-// // };
-// // n.forEach(abc); // .forEach() does not return anything
-
-// const newArray = n.map(function(element) {
-//   return element + 10;
-// }); // .map() returns a new array
-// console.log(newArray);
-
 // const response = {
 //   count: 2,
 //   data: [{
@@ -125,11 +13,13 @@ console.log(greet(person));
 //   return element.name === 'Han Solo'
 // };
 
+
 // const hanssolofilms = response.data.filter(filterFn);
 // console.log(hanssolofilms);
 
-// const numbers = [1, 2, 3, 4];
-// return an array of numbers that can be divided by 2
+const numbers = [1, 2, 3, 4];
+// // return an array of numbers that can be divided by 2
+
 // const filterFun = function(el) {
 //   return el % 2 === 0
 // }
@@ -137,6 +27,94 @@ console.log(greet(person));
 // console.log(divisibleBy2);
 
 // const sum = numbers.reduce(function(prev, current) {
-//   return prev * current
+//   console.log(prev, current)
+//   return prev + current
 // })
 // console.log(sum);
+
+// const numbers = [1, 2, 3];
+// const mapperFunction = function(elements) {
+//   return elements + 5
+// };
+// const newNumbers = numbers.map(mapperFunction);
+// console.log(newNumbers)
+
+// const numbers = [0, 1, 2];
+// const mappingFunction = function(number) {
+//   return number * number;
+// };
+// const mappingFunction = number => number * number;
+
+// const newArray = numbers.map(mappingFunction);
+// console.log(newArray);
+
+// const x = () => 'Hello'; // implicit return
+
+// const x = function() {
+//   return 'Hello';
+// }
+
+// const y = a => a; // implicit return
+
+// const y = function(a) {
+//   return a;
+// };
+
+// const z = (a, b = 1) => a + b; // implicit return
+
+// const z = function(a, b = 1) {
+//   return a + b;
+// };
+
+// const m = function(a, b) {
+//   if (a < 1) {
+//     return a + b
+//   }
+//   return a - b;
+// };
+
+// const m = (a, b) => { // explicit return
+//   if (a < 1) {
+//     return a + b;
+//   }
+//   return a - b;
+// }
+// const greet = function(name, age) {
+//   return {
+//     name,
+//     age
+//   }
+// }
+// const greet = (name, age) => {
+//   return {
+//     name,
+//     age
+//   }
+// }
+
+// const greet = (name, age) => ({ // implicit return
+//   name,
+//   age
+// });
+
+// console.log(
+//   greet('Steve', 18)
+// );
+
+// const quotient = {
+//   numbers: [1, 2, 3, 4, 5, 6, 7],
+//   results: [],
+//   divideFn: function(divisor) {
+//     return this.numbers.map((number) => {
+//       if (number % divisor === 0) {
+//         console.log(this)
+//         return this.results.push(number);
+//       }
+//     })
+//   }
+// };
+// quotient.divideFn(3);
+// console.log(quotient.results);
+// // const greet = (name, age) => {
+// //   return name.toUpperCase()
+// // };
