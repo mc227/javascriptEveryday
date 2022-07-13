ba2be05ec8c21ff235bc818e1c61557a19924857
mc227 <markcostales.mark@gmail.com>
2022-07-13 18:46:21 -0400

    try catch

--- script.js
+++ script.js
@@ -9,33 +9,33 @@ let x;
   x = 1;
   console.log("first block", x);
 }
-// {
-//   x = 2;
-//   console.log("second block", x);
-// }
-// x = 3;
-// console.log("outside the block", x);
+{
+  x = 2;
+  console.log("second block", x);
+}
+x = 3;
+console.log("outside the block", x);
 
-// if (1 === 1) {
-//   let anotherNumber = 2
+if (1 === 1) {
+  let anotherNumber = 2
   
-// }
+}
 
 // console.log(anotherNumber);
 
-// {
-//   let y = 10;
-//   console.log(y);
+{
+  let y = 10;
+  console.log(y);
 //   let y = 20; // Uncaught SyntaxError: Identifier 'y' has already been declared
-//   // y = 20;
-//   console.log(y);
-// }
+  // y = 20;
+  console.log(y);
+}
 
 
 // let x = 1;
-// try {
-//   console.log('X in try', x)
-//   throw new Error('error')
-// } catch (error) {
-//   console.log('X in catch', x)
-// }
+try {
+  console.log('X in try', x)
+  throw new Error('error')
+} catch (error) {
+  console.log('X in catch', x)
+}
